9355cfd72daa24608a4283009b819a108d1b21c0
JustinnLy <65483959+JustinnLy@users.noreply.github.com>
2021-01-23 17:27:32 -0500

    Simple Day Handling

--- CalendarScript.js
+++ CalendarScript.js
@@ -1,12 +1,40 @@
 console.log(window.localStorage);
 
+//Gets the current year and month
+const date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+
+//Gets first and last day(0-6) of a month
+let firstDay = new Date(year,month,1).getDay();
+//Gets the last Date(0-31) of a month
+let lastDay = new Date(year,month+1,0).getDate();
+
+//Shifts the calendar days to match the month
+const firstDayElement = document.getElementById("one");
+firstDayElement.style.gridColumn = firstDay + 1;
+
+//Displays the correct number of days of a month
+if(lastDay == 28){
+    document.getElementById("twoNine").style.display = "none";
+    document.getElementById("threeZero").style.display = "none";
+    document.getElementById("threeOne").style.display = "none";
+}
+else if(lastDay == 29){
+    document.getElementById("threeZero").style.display = "none";
+    document.getElementById("threeOne").style.display = "none";  
+}
+else if(lastDay == 30){
+    document.getElementById("threeOne").style.display = "none"; 
+}
+
 function gotoList (element) {
-    const month = document.getElementById("month").innerHTML;
-    const year = document.getElementById("year").innerHTML;
-    const day = element.innerHTML;
-    window.localStorage.setItem("month",month);
-    window.localStorage.setItem("year",year);
-    window.localStorage.setItem("day",day);
+    const calendarMonth = document.getElementById("month").innerHTML;
+    const calendarYear = document.getElementById("year").innerHTML;
+    const calendarDay = element.innerHTML;
+    window.localStorage.setItem("month",calendarMonth);
+    window.localStorage.setItem("year",calendarYear);
+    window.localStorage.setItem("day",calendarDay);
 
     window.location.href="To_Do_List.html";
 }
