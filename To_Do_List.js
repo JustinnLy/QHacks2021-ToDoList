09f2e0045f64a166b5489d6e04b0e8889f70c451
angelo-truong <59120978+angelo-truong@users.noreply.github.com>
2021-01-23 15:54:29 -0500

    Updated To_Do_List
    added feature where user cannot enter "null" or duplicate tasks
    
    Added KeyEventLister for textbox using "Enter"

--- To_Do_List.js
+++ To_Do_List.js
@@ -4,23 +4,46 @@ console.log(window.localStorage);
 const headerDate = document.getElementById("date");
 headerDate.innerHTML = window.localStorage.month + " " + window.localStorage.day + " " + window.localStorage.year;
 
+document.getElementById("task1").addEventListener("keypress", function (e){
+    if(e.key === "Enter"){
+        updateTask();
+    }});
 function updateTask () {
-    
+    //gets task from textbox    
     const Task = document.getElementById("task1").value;
+    //checks if the user entered nothing
+    if(Task == ''){
+        alert('please Enter a Task!');
+        return;
+    }
 
+    //gets Task Left to be Completed column
     const ToBeDone = document.getElementById("tobedone");
+    
+    //makes a new div, p and checkbox element
     const newTask = document.createElement("div");
     const taskName = document.createElement("p");
     const checkBox = document.createElement("input");
     checkBox.type="checkbox";
     checkBox.id="check";
     taskName.textContent = Task;
-    newTask.classList.add("textbox");
 
-    newTask.appendChild(checkBox);
-    newTask.appendChild(taskName);
-    ToBeDone.appendChild(newTask);
-   
+
+    newTask.classList.add("textbox");
+    var taskList = document.querySelectorAll('p');
+
+    //checks if task name is already on the list
+    for(var i =0; i<taskList.length;i++){
+        if(taskList[i].innerHTML === taskName.innerHTML){
+            alert("Task is already on the list!");
+            return;
+        }
+    }
+    //if not on list then add task name to the list
+        newTask.appendChild(checkBox);
+        newTask.appendChild(taskName);
+        ToBeDone.appendChild(newTask);
+    
 
 }
 
