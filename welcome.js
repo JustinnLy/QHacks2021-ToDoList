297ab21e373e6202c02d661495eeb362f6761d4a
JustinnLy <65483959+JustinnLy@users.noreply.github.com>
2021-01-23 21:44:07 -0500

    Updated welcome page

--- welcome.js
+++ welcome.js
@@ -7,7 +7,22 @@ let dayNum = date.getDate();
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
+//Updates HTML to get current date
 document.getElementById("day").innerHTML = days[day];
 document.getElementById("month").innerHTML = months[month];
 document.getElementById("numDay").innerHTML = dayNum;
 document.getElementById("year").innerHTML = year;
+
+let taskToday = `taskList${months[month]} ${dayNum} ${year}`;
+
+//Prints the x amount of tasks to do for the current day
+if(typeof(window.localStorage[taskToday]) !== "undefined" && window.localStorage[taskToday] !== ""){
+    let taskArray = window.localStorage[taskToday].split(",");
+
+    if(taskArray.length == 1){
+        document.getElementById("todo").innerHTML = "<span>1</span> task to do today";
+    }
+    else{
+        document.getElementById("todoNum").innerHTML = taskArray.length;
+    }
+}
